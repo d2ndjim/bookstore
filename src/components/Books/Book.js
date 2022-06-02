100951b43cd7d943b2ebf911781a818d1e883695
Lekan <jimoridwanlekan@gmail.com>
2022-06-02 12:39:27 +0100

    add book component

---
+++ src/components/Books/Book.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Book = () => {
+  return (
+    <div className="book">
+      <h2>First Book</h2>
+      <button type="button" className="remove-btn">Remove</button>
+    </div>
+  )
+}
+
+export default Book
---
+++ src/components/Books/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <Form>
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Author" />
+      <Button type="button" className="add-btn">Add Book</Button>
+    </Form>
+  );
+}
