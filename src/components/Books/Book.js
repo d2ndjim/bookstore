eac9149566db99158d3fb0cdcd706a622e6274b5
Lekan <jimoridwanlekan@gmail.com>
2022-06-07 14:02:36 +0100

    fix linters issue

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -6,23 +6,15 @@ const Book = () => {
   const books = useSelector(selectBooks);
   const dispatch = useDispatch;
 
-  const handleRemove = (id, e) => {
-    e.preventDefault();
-    dispatch(removeBook(id));
-  };
-
-  // useEffect(() => {
-  // }, []);
-
   return (
     <div className="book">
       <ul>
         {books.map((book) => (
-          <li id="book.id" key={book.id}>
+          <li id={book.id} key={book.id}>
             <p>{book.category}</p>
             <p>{book.title}</p>
             <p>{book.author}</p>
-            <button type="button" onClick={(e) => handleRemove(book.id, e)}>Remove Book</button>
+            <button type="button" onClick={dispatch(removeBook(book.id))}>Remove Book</button>
           </li>
         ))}
       </ul>
